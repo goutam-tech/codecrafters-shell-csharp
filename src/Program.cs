6119e70cf1fffc0b91c9e26b24d9b25fee979ca7
goutam-tech <gevil463@gmail.com>
2026-08-28 12:23:30 +0530

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -74,6 +74,40 @@ class Program
         }
     }
 
+    //static void ExecuteExternalCommand(string[] parts)
+    //{
+    //    string command = parts[0];
+
+    //    string? executable = FindExecutable(command);
+
+    //    if (executable == null)
+    //    {
+    //        Console.WriteLine($"{command}: command not found");
+    //        return;
+    //    }
+
+    //    var process = new Process();
+
+    //    process.StartInfo.FileName = "/bin/bash";
+    //    process.StartInfo.UseShellExecute = false;
+
+    //    process.StartInfo.ArgumentList.Add("-c");
+
+    //    process.StartInfo.ArgumentList.Add("exec \"$1\" \"${@:2}\"");
+
+    //    process.StartInfo.ArgumentList.Add(command);
+
+    //    process.StartInfo.ArgumentList.Add(executable);
+
+    //    for (int i = 1; i < parts.Length; i++)
+    //    {
+    //        process.StartInfo.ArgumentList.Add(parts[i]);
+    //    }
+
+    //    process.Start();
+
+    //    process.WaitForExit();
+    //}
     static void ExecuteExternalCommand(string[] parts)
     {
         string command = parts[0];
@@ -92,8 +126,7 @@ class Program
         process.StartInfo.UseShellExecute = false;
 
         process.StartInfo.ArgumentList.Add("-c");
-
-        process.StartInfo.ArgumentList.Add("exec \"$1\" \"${@:2}\"");
+        process.StartInfo.ArgumentList.Add("exec -a \"$0\" \"$1\" \"${@:2}\"");
 
         process.StartInfo.ArgumentList.Add(command);
 
