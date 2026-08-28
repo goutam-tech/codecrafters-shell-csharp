38b3565a3eae16834b3feb016a3928297226d97b
goutam-tech <gevil463@gmail.com>
2026-08-28 11:46:12 +0530

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -3,5 +3,7 @@ class Program
     static void Main()
     {
         Console.Write("$ ");
+        string command = Console.ReadLine();
+        Console.WriteLine($"{command}: command not found");
     }
 }
