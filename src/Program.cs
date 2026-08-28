0418963860df18c319c5854e0ece601737e51beb
goutam-tech <gevil463@gmail.com>
2026-08-28 11:48:51 +0530

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -2,8 +2,11 @@ class Program
 {
     static void Main()
     {
-        Console.Write("$ ");
-        string command = Console.ReadLine();
-        Console.WriteLine($"{command}: command not found");
+        while (true)
+        {
+            Console.Write("$ ");
+            var command = Console.ReadLine();
+            Console.WriteLine($"{command}: command not found");
+        }
     }
 }
